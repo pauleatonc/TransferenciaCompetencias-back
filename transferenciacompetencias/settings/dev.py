ef2e753a073a1ef5be774c1156da66395441e422
Paul Eaton <paul.eaton@subdere.gov.cl>
2023-12-21 12:19:22 -0300

    QA Server Configuration

---
+++ transferenciacompetencias/settings/dev.py
@@ -0,0 +1,61 @@
+import environ
+import os
+from .base import *
+
+# we load the variables from the .env file to the environment
+env = environ.Env()
+environ.Env.read_env()
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# Your secret key
+SECRET_KEY = env("SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env.bool('DEBUG', default=False)
+
+ALLOWED_HOSTS = [ 'qatc.subdere.gob.cl', ]
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    # Base de datos de aplicación
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': 'Transferenciadb',
+        'PORT': env("DB_PORT"),
+    }
+        # Base de datos externa
+        # Revisar cómo setiaremos la base de datos externa en ambiente de desarrollo. Para poder testear la importación de usuarios.
+
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'transferenciacompetencias')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/tc')
+MEDIA_URL = '/media/'
+
+RECAPTCHA_PRIVATE_KEY = env("RECAPTCHA_PRIVATE_KEY")
+RECAPTCHA_PUBLIC_KEY = env("RECAPTCHA_PUBLIC_KEY")
+
+# Sendgrid
+SENDGRID_API_KEY = env("SENDGRID_API_KEY")
+ADMIN_EMAIL = env("ADMIN_EMAIL")
+NOREPLY_EMAIL = env("NOREPLY_EMAIL")
+
+
+# Trusted origins for the CSRF validation
+# https://docs.djangoproject.com/en/4.2/ref/settings/#csrf-trusted-origins
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[])
+
+# If we are behind proxy with https we trust the header defined here.
+# https://docs.djangoproject.com/en/4.2/ref/settings/#secure-proxy-ssl-header
+SECURE_PROXY_SSL_HEADER = env.tuple('SECURE_PROXY_SSL_HEADER', default=None)
