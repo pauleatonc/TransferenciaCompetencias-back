96c57b6c43ea82e0613b62b9a17350cc533aba00
JaimeHernandezE <jhearquitecto@gmail.com>
2024-03-08 11:42:29 -0300

    FIX: Migraciones para recargar modelos desde Excel

---
+++ applications/formularios_sectoriales/migrations/0048_asignar_subtitulos_desde_excel.py
@@ -0,0 +1,37 @@
+from django.db import migrations
+import pandas as pd
+from pathlib import Path
+
+
+def populate_subtitulos_e_items_from_excel(apps=None, schema_editor=None):
+    if apps is None:
+        from applications.formularios_sectoriales.models import Subtitulos, ItemSubtitulo
+    else:
+        Subtitulos = apps.get_model('formularios_sectoriales', 'Subtitulos')
+        ItemSubtitulo = apps.get_model('formularios_sectoriales', 'ItemSubtitulo')
+
+    file_path = Path(__file__).resolve().parent.parent / 'Clasificador_Presupuestario.xlsx'
+    df = pd.read_excel(file_path, header=None)
+
+    for column in df.columns:
+        subtitulo_name = df.iloc[0, column]
+        items = df.iloc[1:, column].dropna().tolist()
+
+        subtitulo, created = Subtitulos.objects.get_or_create(subtitulo=subtitulo_name)
+
+        for item in items:
+            ItemSubtitulo.objects.get_or_create(
+                item=item,
+                subtitulo=subtitulo,
+            )
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('formularios_sectoriales', '0047_alter_personalindirecto_options'),
+    ]
+
+    operations = [
+       migrations.RunPython(populate_subtitulos_e_items_from_excel)
+    ]
