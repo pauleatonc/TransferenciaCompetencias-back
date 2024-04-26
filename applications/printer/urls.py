bfd4eed678e853317c3386fd0a9f30715392f3b2
JaimeHernandezE <jhearquitecto@gmail.com>
2024-04-26 09:29:37 -0400

    FIX: Agregado índice y paginación a pdf final

--- applications/printer/urls.py
+++ applications/printer/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('preview/resumen/<int:competencia_id>/', views.resumen_competencia, name='competencia'),
-    path('preview/formulario-sectorial/<int:formulario_sectorial_id>/', views.formulario_sectorial_paso1, name='formulario-sectorial_paso1'),
+    path('preview/formulario-sectorial_paso1/<int:formulario_sectorial_id>/', views.formulario_sectorial_paso1, name='formulario-sectorial_paso1'),
+    path('preview/formulario-sectorial_paso2/<int:formulario_sectorial_id>/', views.formulario_sectorial_paso2, name='formulario-sectorial_paso2'),
     path('download/complete-document/<int:competencia_id>/', views.download_complete_document, name='complete-document'),
 ]
