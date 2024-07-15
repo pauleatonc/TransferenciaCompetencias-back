06aea9aefa4da014a664d796b4416255eeff8aa1
JaimeHernandezE <jhearquitecto@gmail.com>
2024-07-15 17:06:07 -0400

    FIX: API revision_final_serializers.py para enviar correctamente temporalidad_gradualidad

--- applications/competencias/signals/revision_final_signals.py
+++ applications/competencias/signals/revision_final_signals.py
@@ -109,8 +109,8 @@ def update_competencia_status(sender, instance, **kwargs):
                 instance.save(update_fields=['recomendacion_transferencia'])
 
             # Generar y guardar el PDF si el formulario final ha sido enviado
-            if instance.formulario_final_enviado:
-                save_complete_document_pdf(instance.id)
+            #if instance.formulario_final_enviado:
+                #save_complete_document_pdf(instance.id)
 
     finally:
         # Quitar marca para permitir futuras actualizaciones
