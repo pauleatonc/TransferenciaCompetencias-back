7ed1ff93272d0dd2f890eb097e2f6c19f00a8f09
JaimeHernandezE <jhearquitecto@gmail.com>
2024-07-18 10:40:03 -0400

    FIX: Correcciones a API para finalizar etapa5

--- applications/etapas/signals/etapa5_signals.py
+++ applications/etapas/signals/etapa5_signals.py
@@ -1,8 +1,9 @@
-from django.db.models.signals import m2m_changed
+from django.db.models.signals import m2m_changed, post_save
 from django.dispatch import receiver
 
 from applications.competencias.models import Competencia
 from applications.etapas.models import Etapa5
+from applications.formularios_gores.models import ObservacionesSubdereFormularioGORE
 
 
 @receiver(m2m_changed, sender=Competencia.usuarios_dipres.through)
@@ -13,3 +14,20 @@ def actualizar_etapa5_al_modificar_usuarios_dipres(sender, instance, action, pk_
             if etapa5:
                 etapa5.usuario_notificado = instance.usuarios_dipres.exists()
                 etapa5.save()
+
+
+@receiver(post_save, sender=ObservacionesSubdereFormularioGORE)
+def comprobar_y_finalizar_observaciones_etapa5(sender, instance, **kwargs):
+    # Comprobar si todas las observaciones han sido enviadas
+    todas_enviadas = all(
+        observacion.observacion_enviada for observacion in
+        ObservacionesSubdereFormularioGORE.objects.filter(
+            formulario_gore__competencia=instance.formulario_gore.competencia)
+    )
+
+    # Obtener Etapa2 relacionada con la competencia
+    etapa5 = Etapa5.objects.filter(competencia=instance.formulario_gore.competencia).first()
+
+    if todas_enviadas and etapa5:
+        etapa5.observacion_minuta_gore_enviada = True
+        etapa5.save()
